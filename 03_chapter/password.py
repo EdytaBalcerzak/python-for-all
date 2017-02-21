134cc4d9e937af19a3b6ea8e209791369d1fcc52
EdytaBalcerzak <edytabalcerzak1@gmail.com>
2017-02-21 15:23:50 +0100

    3rd chapter 1st look

---
+++ 03_chapter/password.py
@@ -0,0 +1,6 @@
+print("Witaj na moim komputerze")
+print("Zeby uzyskac dostep")
+password = input("Wprowadz haslo")
+if password == "zgredek":
+    print("Haslo prawidlowe.. Dostep uzyskany")
+input("aby zakonczyc program, nacisnij Enter")
---
+++ 03_chapter/rzut_kostka.py
@@ -0,0 +1,10 @@
+print("\t\t\tRzut koscmi")
+
+import random
+
+kostka1 = random.randint(1, 6)
+kostka2 = random.randrange(6) + 1
+
+total = kostka1 + kostka2
+print("Wyrzuciles", kostka1, "oraz", kostka2, "\nUzyskales sume", total, "oczek")
+input("aby zakonczyc program nacisnij Enter")
