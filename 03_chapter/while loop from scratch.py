bf5fdb5356aeb0e4a051746f3f8b9a35405033d4
EdytaBalcerzak <edytabalcerzak1@gmail.com>
2017-02-28 23:07:41 +0100

    Revert "Revert "przekupny kelner""
    This reverts commit 1e8105b0d5e3d4c017faaaf0a667e633dd1a57a3.

---
+++ 03_chapter/while loop from scratch.py
@@ -0,0 +1,7 @@
+print("nauka petli od podstaw\n\n")
+
+response = ""
+while response != "czego nie wiesz":
+    response = input(" Nie wiem  ")
+print(" niczego")
+input("\n\naby zakonczyc program, nacisnij Enter")
